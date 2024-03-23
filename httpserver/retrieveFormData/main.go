5f752ee8622440911439ed58dcb3a5f7598d3297
Niharika_Attri <niharikaattri2005@gmail.com>
2024-03-23 16:31:16 +0530

    form data

---
+++ httpserver/retrieveFormData/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+const keyServerAddr = "serverAddr"
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request \n", ctx.Value(keyServerAddr))
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		myName = "HTTP"
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+
+	ctx := context.Background()
+	server := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("server closed\n")
+	} else if err != nil {
+		fmt.Println("error listening for server: %s\n", err)
+	}
+}
